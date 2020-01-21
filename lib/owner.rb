77c1b7cf7932426289470c2da562927536452ca6
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-21 03:36:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,7 +52,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+    "I have  #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end
 
   def sell_pets
